f95112934e6e8dcd938aebfc57def50054c5fc94
LCN1993 <snilove@hotmail.com>
2019-01-31 10:57:24 +0800

    Initial Commit

---
+++ Chat-Version2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat-Version2
+//
+//  Created by Ethan  on 31/1/19.
+//  Copyright © 2019 Ethan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
